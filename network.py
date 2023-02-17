07606fd18bdf641f965bb8fcc14c6313267a9c7b
tommasopardi <tommasopardi@automata.tech>
2023-02-17 23:17:19 +0000

    feat{network}: Added generator network and subsidiary function to de-blot the script

--- network.py
+++ network.py
@@ -43,3 +43,51 @@ class Discriminator(Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.d_block(x)
         return x
+
+
+class BlockDeconv(Module):
+    def __init__(self, input_channels, output_channels, kernel_size=3, padding=0, stride=2, batch_norm=False,
+                 activation=True):
+        super(BlockDeconv, self).__init__()
+
+        self.deconv = nn.ConvTranspose2d(input_channels, output_channels, kernel_size=kernel_size, padding=padding,
+                                         stride=stride, bias=False)
+        self.bn2d = nn.BatchNorm2d(output_channels)
+
+        self.activation = nn.ReLU()
+        self.batch_norm = batch_norm
+        self.activation_flag = activation
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        x = self.deconv(x)
+
+        if self.batch_norm:
+            x = self.bn2d(x)
+
+        if self.activation_flag:
+            x = self.activation(x)
+
+        return x
+
+
+class Generator(Module):
+    def __init__(self, latent_dim: int, conv_features: int = 64):
+        super(Generator, self).__init__()
+
+        self.conv_features = conv_features
+        self.latent_dim = latent_dim
+
+        self.g_block = nn.Sequential(
+            BlockDeconv(self.latent_dim, self.conv_features * 8, kernel_size=4, padding=0, batch_norm=False),
+            BlockDeconv(self.conv_features * 8, self.conv_features * 4, kernel_size=4, padding=1, batch_norm=True),
+            BlockDeconv(self.conv_features * 4, self.conv_features * 2, kernel_size=4, padding=1, batch_norm=True),
+            BlockDeconv(self.conv_features * 2, self.conv_features, kernel_size=4, padding=1, batch_norm=True),
+            BlockDeconv(self.conv_features, 3, kernel_size=4, padding=1, batch_norm=False, activation=False),
+            nn.Tanh())
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        x = self.g_block(x)
+
+        return x
