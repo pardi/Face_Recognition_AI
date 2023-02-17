427e9489ae21881e92627a691911d2343d802472
tommasopardi <tommasopardi@automata.tech>
2023-02-17 22:56:47 +0000

    chore(utils): create a couple of functions to debug and visualise the results

---
+++ utils/common.py
@@ -0,0 +1,21 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+
+def denormalize(images):
+    """Transform images from [-1.0, 1.0] to [0, 255] and cast them to uint8."""
+    return ((images + 1.) / 2. * 255).astype(np.uint8)
+
+
+def display(fixed_latent_vector: torch.Tensor):
+    """ Helper function to display images during training """
+    fig = plt.figure(figsize=(14, 4))
+    plot_size = 16
+    for idx in np.arange(plot_size):
+        ax = fig.add_subplot(2, int(plot_size/2), idx+1, xticks=[], yticks=[])
+        img = fixed_latent_vector[idx, ...].detach().cpu().numpy()
+        img = np.transpose(img, (1, 2, 0))
+        img = denormalize(img)
+        ax.imshow(img)
+    plt.show()
